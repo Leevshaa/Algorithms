46400327fbaac90eb3c89918765bc8e825135fff
Vitalii <vitaliti.yurchenko@gmail.com>
2024-08-08 18:32:57 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,16 @@
+package app;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 23, 69, 30, 18,
+                83, 5, 68, 1, 7};
+
+        System.out.println("Початковий вигляд масиву: " + Arrays.toString(numbers));
+
+
+    }
+}
